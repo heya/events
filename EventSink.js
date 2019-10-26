1d4d9867f3f4f1f918b4853db20905f00e6faeb2
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2019-10-25 22:32:47 -0500

    Updated dependencies.

--- EventSink.js
+++ EventSink.js
@@ -1,5 +1,5 @@
 /* UMD.define */ (typeof define=="function"&&define||function(d,f,m){m={module:module,require:require};module.exports=f.apply(null,d.map(function(n){return m[n]||require(n)}))})
-(["./EventSource", "dcl/mini"], function(EventSource, dcl){
+(["./EventSource", "dcl"], function(EventSource, dcl){
 	"use strict";
 
 	var Value = EventSource.Value, ErrorValue = EventSource.ErrorValue,
